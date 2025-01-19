da8098777e7166437bd5ecc1238b629d37369a14
Ke Ning <kn2132@nyu.edu>
2025-01-19 20:16:15 +0800

    config update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/global-show-tell/',  // GitHub Pages repo name
+  base: 'global-show-tell',  // GitHub Pages repo name
 });
